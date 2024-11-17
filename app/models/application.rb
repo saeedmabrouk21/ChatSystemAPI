9cb9431b2cbb137a15e49ffb8428ef62a37ba44e
saeedmabrouk21 <saeedmabrouk21@gmail.com>
2024-11-17 23:49:03 +0200

    change redis chat_count redit intializtion callback to chat_max_number to eliminate the conflict

--- app/models/application.rb
+++ app/models/application.rb
@@ -5,18 +5,18 @@ class Application < ApplicationRecord
   validates :token, presence: true, uniqueness: true
   validates :name, presence: true
 
-  before_create :initialize_chat_count_in_redis
+  before_create :initialize_chat_max_number_in_redis
 
 
   def generate_chat_number
     begin
-      Redis.current.watch("application:#{self.token}:chats_max_number") do
-        current_count = Redis.current.get("application:#{self.token}:chats_max_number").to_i
+      Redis.current.watch("application:#{self.id}:chats_max_number") do
+        current_count = Redis.current.get("application:#{self.id}:chats_max_number").to_i
         new_count = current_count + 1
 
         # Use MULTI to start a transaction and execute the set atomically
         Redis.current.multi do
-          Redis.current.set("application:#{self.token}:chats_max_number", new_count)
+          Redis.current.set("application:#{self.id}:chats_max_number", new_count)
         end
 
         return new_count # Return if the Redis operation succeeds
@@ -32,8 +32,8 @@ class Application < ApplicationRecord
 
   private
 
-  def initialize_chat_count_in_redis
+  def initialize_chat_max_number_in_redis
     # Initialize the Redis counter for chats to 0 when the application is created
-    Redis.current.set("application:#{self.token}:chats_max_number", 0)
+    Redis.current.set("application:#{self.id}:chats_max_number", 0)
   end
 end
