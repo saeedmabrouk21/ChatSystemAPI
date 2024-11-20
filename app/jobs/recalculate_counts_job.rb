021ed683379a72aac712e6592b003e1b8c512e57
saeedmabrouk21 <saeedmabrouk21@gmail.com>
2024-11-20 22:02:43 +0200

    added logged logs in RecalculateCountsJob

--- app/jobs/recalculate_counts_job.rb
+++ app/jobs/recalculate_counts_job.rb
@@ -8,12 +8,14 @@ class RecalculateCountsJob
     Application.find_each do |application|
       new_chats_count = application.chats.count
       application.update(chats_count: new_chats_count)
+      Rails.logger.info "Updated chats_count for Application ##{application.id}: #{new_chats_count}"
     end
 
     # Recalculate messages_count for each chat
     Chat.find_each do |chat|
       new_messages_count = chat.messages.count
       chat.update(messages_count: new_messages_count)
+      Rails.logger.info "Updated messages_count for Chat ##{chat.id}: #{new_messages_count}"
     end
   end
 end
