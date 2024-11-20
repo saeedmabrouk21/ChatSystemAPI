7edb2c833eaad45ef045ae2cf3529be8d5c3ca3b
saeedmabrouk21 <saeedmabrouk21@gmail.com>
2024-11-20 20:43:08 +0200

    move the chat number genrating logic into the model

--- app/models/application.rb
+++ app/models/application.rb
@@ -8,29 +8,9 @@ class Application < ApplicationRecord
   before_create :initialize_chat_max_number_in_redis
   before_validation :assign_unique_token, on: :create
 
-
   def generate_chat_number
-    begin
-      Redis.current.watch("application:#{self.id}:chats_max_number") do
-        current_count = Redis.current.get("application:#{self.id}:chats_max_number").to_i
-        new_count = current_count + 1
-
-        # Use MULTI to start a transaction and execute the set atomically
-        Redis.current.multi do
-          Redis.current.set("application:#{self.id}:chats_max_number", new_count)
-        end
-
-        return new_count # Return if the Redis operation succeeds
-      end
-    rescue Redis::WatchError
-      retries += 1
-      retry if retries < Rails.configuration.max_retries
-    end
-
-    # Fallback if Redis fails after retries
-    self.chats.maximum(:number).to_i + 1
+    NumberGeneratorService.new(self, nil).generate_chat_number
   end
-
   private
 
   def initialize_chat_max_number_in_redis
--- app/services/number_generator_service.rb
+++ app/services/number_generator_service.rb
@@ -4,4 +4,30 @@ class NumberGeneratorService
     @application = application
     @chat = chat
   end
+
+  def generate_chat_number
+    return generate_number_for_chat(@application) if @application
+    raise "Application is required for generating chat number."
+  end
+
+  private
+  def generate_number_for_chat(application)
+    begin
+      Redis.current.watch("application:#{application.id}:chats_max_number") do
+        current_count = Redis.current.get("application:#{application.id}:chats_max_number").to_i
+        new_count = current_count + 1
+
+        Redis.current.multi do
+          Redis.current.set("application:#{application.id}:chats_max_number", new_count)
+        end
+
+        return new_count
+      end
+    rescue Redis::WatchError
+      retry if retries < Rails.configuration.max_retries
+    end
+
+    # Fallback to finding the max chat number in the database
+    application.chats.maximum(:number).to_i + 1
+  end
 end
